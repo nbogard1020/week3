9fdd760df79f08ac398e35c415cb2d5e0921cb69
nbogard1020 <65126053+nbogard1020@users.noreply.github.com>
2020-05-21 13:30:23 -0500

    Add files via upload

---
+++ Pizza.py
@@ -0,0 +1,15 @@
+class Pizza:
+
+    Attributes: self.crust, self.cheese, self.sauce
+
+class Pepperoni(Pizza): 
+
+    Attributes: self.pepperoni, self.redflakes, self.parmesan = "Pepperoni"
+
+class Sausage(Pizza): 
+
+    Attributes: self.sausage, self.italianseasoning, self.parmesan = "Sausage"
+
+class Veggie(Pizza): 
+
+    Attributes: self.peppers, self.onions, self.mushrooms, self.olives
